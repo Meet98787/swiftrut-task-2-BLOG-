866fb9e438090ad72fa4880810c496adf678e2ee
Meet98787 <127495492+Meet98787@users.noreply.github.com>
2024-09-17 23:16:26 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:5000/api'; // Change this to your backend URL
 
-export const getPosts = () => axios.get(`http://localhost:5000/api/posts`);
-export const getPostById = (id) => axios.get(`http://localhost:5000/api/posts/${id}`);
-export const createPost = (post) => axios.post(`http://localhost:5000/api/posts`, post);
-export const updatePost = (id, updatedPost) => axios.put(`http://localhost:5000/api/posts/${id}`, updatedPost);
-export const deletePost = (id) => axios.delete(`http://localhost:5000/api/posts/${id}`);
+export const getPosts = () => axios.get(`https://swiftrut-task-2-blog.vercel.app/api/posts`);
+export const getPostById = (id) => axios.get(`https://swiftrut-task-2-blog.vercel.app/api/posts/${id}`);
+export const createPost = (post) => axios.post(`https://swiftrut-task-2-blog.vercel.app`, post);
+export const updatePost = (id, updatedPost) => axios.put(`https://swiftrut-task-2-blog.vercel.app/api/posts/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`https://swiftrut-task-2-blog.vercel.app/api/posts/${id}`);
